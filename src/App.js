7a496a8baa6538aa2d9154c11f0a86fc7e49dca6
AdityaNair07 <nairaditya07@gmail.com>
2023-05-29 14:58:43 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+import Todolist from "./Todolist";
 
-function App() {
+const App = () => {
+  const [inputList, setInputList] = useState("");
+
+  const [Items, setItems] = useState([]);
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
+
+  const listOfItems = () => {
+    if (inputList.length === 0) {
+      return;
+    }
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    setInputList("");
+  };
+
+  const deleteItems = (id) => {
+    console.log("deleted");
+    setItems((oldItems) => {
+      return oldItems.filter((arrElement, index) => {
+        return index !== id;
+      });
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>
+            ToDo List <i className="fa fa-check-circle" aria-hidden="true"></i>
+          </h1>
+          <br />
+          <input
+            type="text"
+            placeholder="Add a task"
+            onChange={itemEvent}
+            value={inputList}
+          />
+          <button onClick={listOfItems}>+</button>
+
+          <ol>
+            {Items.map((itemValue, index) => {
+              return (
+                <Todolist
+                  text={itemValue}
+                  key={index}
+                  id={index}
+                  onSelect={deleteItems}
+                />
+              );
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
